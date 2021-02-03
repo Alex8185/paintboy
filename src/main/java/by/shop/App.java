e03138b207b40908c777ca4f14caabd635bb6ed1
Alex8185 <eclipse85@tut.by>
2021-02-03 18:12:46 +0300

    checked-exception-exercise-1

--- src/main/java/by/shop/App.java
+++ src/main/java/by/shop/App.java
@@ -27,15 +27,29 @@ public class App {
                     System.out.println((a + 1) + ". " + items[a]);
                 }
                 admin.speak("Пожалуйста, сделайте выбор");
-                String productNumber = scanner.nextLine();
-                int opNumber = Integer.parseInt(productNumber);
-                seller.speak(items[opNumber-1] + " Это отличный выбор");
-                System.exit(0);
 
-            } else if (line.equals("Нет")) {
+                int attemptNumberItems = 2;
+                for (int b = 0; b < attemptNumberItems; b++) {
+                String productNumber = scanner.nextLine();
+
+                try {
+                    int opNumber = Integer.parseInt(productNumber);
+                    seller.speak(items[opNumber - 1] + " Это отличный выбор");
+                    System.exit(0);
+                } catch (NumberFormatException e) {
+                    admin.speak("Значение не верно, введите корректное значение");
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    admin.speak("Введите корректное значение или закончите покупки");
+                    if (b < 1)
+                        continue;
+                    scanner.nextLine();
+                }}
+
+            } else if (line.equalsIgnoreCase("Нет")) {
                 client.speak("Извините, хорошего дня!");
                 seller.speak("Спасибо, и Вам хорошего дня!");
                 System.exit(0);
+                scanner.close();
             } else {
                 if (i < 3) {
                     admin.speak("Не корректный ответ. Попробуйте еще раз");
